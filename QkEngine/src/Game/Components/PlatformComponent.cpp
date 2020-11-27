10b47e50610e83af1ac3bd3d02fbecc796bc7ee1
Ja Fajny <jendromij@gmail.com>
2020-11-27 23:04:46 +0100

    Now PlatformComponent moves between 2 waypoints

--- QkEngine/src/Game/Components/PlatformComponent.cpp
+++ QkEngine/src/Game/Components/PlatformComponent.cpp
@@ -26,13 +26,20 @@ std::unique_ptr<Component> PlatformComponent::MakeCopy(GameObject* newParent) co
 
 void PlatformComponent::Update(const float& deltaTime)
 {
-	
+	if (parent->transform.MoveTo(waypoints[currWaypoint], moveSpeed * deltaTime))
+	{
+		currWaypoint++;
+
+		if (currWaypoint >= waypoints.size())
+			currWaypoint = 0;
+	}
 }
 
 void PlatformComponent::ShowOnInspector()
 {
 	ImGui::DragFloat3("PointA", &waypoints[0].x, 0.1f);
 	ImGui::DragFloat3("PointB", &waypoints[1].x, 0.1f);
+	ImGui::DragFloat("Move Speed", &moveSpeed, 0.1f);
 }
 
 void PlatformComponent::ShowOnGizmos()
